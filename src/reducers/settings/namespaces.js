c000d7c404d63829380ecd1d70b651aa52efca64
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2025-07-26 22:11:26 +0200

    Update namespaces.js

--- src/reducers/settings/namespaces.js
+++ src/reducers/settings/namespaces.js
@@ -2,7 +2,8 @@ import * as TYPE from 'actions/types';
 
 const initialState = {
     extNamespace: 'distordia', // Set your default following here
-    myNamespace: '' // Set your default namespace here
+    myNamespace: '', // Set your default namespace here
+    selectedNamespace: null // Currently selected namespace for filtering
     };
 
 export default (state = initialState, action) => {
@@ -16,6 +17,11 @@ export default (state = initialState, action) => {
       return {
         ...state,
         myNamespace: action.payload,
+      };
+    case TYPE.SET_SELECTED_NAMESPACE:
+      return {
+        ...state,
+        selectedNamespace: action.payload,
       };
     default:
       return state;
